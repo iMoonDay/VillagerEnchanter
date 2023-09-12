e0af3ff09eb60e3ff2079aaa6a4514a1eb1c8a10
iMoonDay <562613736@qq.com>
2023-09-12 17:54:54 +0800

    v0.1

---
+++ src/main/java/com/imoonday/villager_enchanter/ModEvents.java
@@ -0,0 +1,34 @@
+package com.imoonday.villager_enchanter;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Mod.EventBusSubscriber(modid = VillagerEnchanter.MODID)
+public class ModEvents {
+    @SubscribeEvent
+    public static void initTrades(VillagerTradesEvent event) {
+        if (event.getType() == VillagerProfession.LIBRARIAN) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(VillagerEnchanter.ENCHANT_REFRESH_BOOK.get(), 1);
+            int villagerLevel = 5;
+            trades.get(villagerLevel).add((entity, random) -> new MerchantOffer(new ItemStack(Items.EMERALD, 16), new ItemStack(Items.BOOK), stack, 4, 15, 0.05f));
+        }
+
+        if (event.getType() == VillagerEnchanter.ENCHANTER_PROFESSION.get()) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            trades.get(1).add((entity, random) -> new MerchantOffer(new ItemStack(Items.LAPIS_LAZULI, 8), new ItemStack(Items.EMERALD), 16, 2, 0.05f));
+            IntStream.of(1, 2, 2, 3, 3, 4, 4, 5).forEach(i -> trades.get(i).add((entity, random) -> new EnchantRefreshBookItem.EnchantBookForEmeralds(15).getOffer(entity, random)));
+            trades.get(5).add((entity, random) -> new MerchantOffer(new ItemStack(Items.EMERALD, Config.bookPrice), new ItemStack(Items.BOOK), new ItemStack(VillagerEnchanter.ENCHANT_REFRESH_BOOK.get()), Config.bookTradeCount, 15, 0.05f));
+        }
+    }
+}
---
+++ src/main/java/com/imoonday/villager_enchanter/VillagerEnchanter.java
@@ -0,0 +1,45 @@
+package com.imoonday.villager_enchanter;
+
+import com.google.common.collect.ImmutableSet;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.ai.village.poi.PoiType;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.Set;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(VillagerEnchanter.MODID)
+public class VillagerEnchanter {
+
+    public static final String MODID = "villager_enchanter";
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final RegistryObject<Item> ENCHANT_REFRESH_BOOK = ITEMS.register("enchant_refresh_book", () -> new EnchantRefreshBookItem(new Item.Properties().stacksTo(16).tab(CreativeModeTab.TAB_MISC)));
+
+    public static final DeferredRegister<PoiType> POI_TYPES = DeferredRegister.create(ForgeRegistries.POI_TYPES, MODID);
+    public static final RegistryObject<PoiType> ENCHANTING_TABLE_POI_TYPE = POI_TYPES.register("enchanter", () -> new PoiType(Set.of(Blocks.ENCHANTING_TABLE.defaultBlockState()), 1, 1));
+
+    public static final DeferredRegister<VillagerProfession> VILLAGER_PROFESSIONS = DeferredRegister.create(ForgeRegistries.VILLAGER_PROFESSIONS, MODID);
+    public static final RegistryObject<VillagerProfession> ENCHANTER_PROFESSION = VILLAGER_PROFESSIONS.register("enchanter", () -> new VillagerProfession("enchanter", poiTypeHolder -> poiTypeHolder.is(ENCHANTING_TABLE_POI_TYPE.getId()), poiTypeHolder -> poiTypeHolder.is(ENCHANTING_TABLE_POI_TYPE.getId()), ImmutableSet.of(), ImmutableSet.of(), SoundEvents.VILLAGER_WORK_LIBRARIAN));
+
+    public VillagerEnchanter() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ITEMS.register(modEventBus);
+        POI_TYPES.register(modEventBus);
+        VILLAGER_PROFESSIONS.register(modEventBus);
+        MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+}
